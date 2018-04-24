5f08ba3d57154a572b71e1a69858e216bfcfa52e
Kisung Lim <kisungqueso@hotmail.com>
2018-04-24 08:51:48 -0600

    Primer Commit del proyecto. ini

---
+++ cmake-build-debug/CMakeFiles/Extraclase_3_DatosII.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Extraclase_3_DatosII.dir/main.cpp.o"
+  "Extraclase_3_DatosII.pdb"
+  "Extraclase_3_DatosII"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Extraclase_3_DatosII.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Extraclase_3_DatosII.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Extraclase_3_DatosII.
+# This may be replaced when dependencies are built.
