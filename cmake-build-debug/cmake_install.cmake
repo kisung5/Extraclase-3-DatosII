0206086ab7763a7d62b6a15943b63141e952fe0d
Kisung Lim <kisungqueso@hotmail.com>
2018-04-24 09:00:10 -0600

    Creacion de subcarpetas de trabajo.

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/kisung/CLionProjects/Extraclase-3-DatosII/Extraclase-3-DatosII
+# Install script for directory: /home/kisung/CLionProjects/Extraclase-3-DatosII
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/kisung/CLionProjects/Extraclase-3-DatosII/Extraclase-3-DatosII/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/kisung/CLionProjects/Extraclase-3-DatosII/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
