563a00908eec35af092d1013e9983b96cf258a49
deibergv <30886676+deibergv@users.noreply.github.com>
2018-04-25 21:31:47 -0600

    Empleado metodo para testo de tiempos
    Solo es cambiar el "LargoDeArray" y el metodo a utilizar (esta que siempre busque el ultimo elemento)

--- main.cpp
+++ main.cpp
@@ -1,44 +1,27 @@
 #include <iostream>
 #include "Interpolation Search/Interpolation.h"
 #include "Binary Search/BinaryS.hpp"
-#include "Sequential Search/SeqS.hpp"
-
+#include "Sequential Search/SeqS.h"
 
 int main() {
-    //INTERPOLATION
-//    int array[] = {1,2,3,4,5};
-//    Interpolation interpolation;
-//    int n = sizeof(array)/sizeof(array[0]);
-//    clock_t time = -clock();
-//    int index = interpolation.interpolationSearch(array,3,n);
-//    time += clock();
-//    double total = time / (double)CLOCKS_PER_SEC;
-//    std::cout << index << std::endl;
-//    std::cout << total;
-//    return 0;
+    int LargoDeArray = 101;
+    int array[LargoDeArray] = {};
+    for (int i=0; i<LargoDeArray; i++) {
+        array[i] = i;
+    }
+    int len = sizeof(array) / sizeof(array[0]);
 
-
-    //BINARY
-//    int arr[] = {1, 2, 2, 3, 4, 6, 7, 8, 9, 0, 12, 13, 14, 15, 11, 22, 33, 44, 55};
-//    auto n = sizeof(arr) / sizeof(arr[0]);
-//    std::sort(arr, arr + (n - 1));
-//    auto time = -clock();
-//    auto index = binarySearch(arr, 0, n - 1, 33);
-//    time += clock();
-//    double total = time / (double)CLOCKS_PER_SEC;
-//    std::cout << index << std::endl;
-//    std::cout << total;
-//    return 0;
-
-
-    //SECUENCIAL
-    int array[] = {1, 2, 3, 4, 5};
-    int n = sizeof(array) / sizeof(array[0]);
     long time = -clock();
-    int index = BusquedaSecuencial(array, 3);
+
+//    int index = BusquedaSecuencial(array, len, LargoDeArray-1); //Secuencial (Lista, largo de lista, Numero a buscar)
+//    int index = binarySearch(array, 0, len, LargoDeArray-1);    //Binaria (Lista, Numero inicial, largo de lista, Numero a buscar)
+    Interpolation interpolation;                                  //Secuencial (Lista, Numero a buscar, largo de lista)
+    int index = interpolation.interpolationSearch(array,LargoDeArray-1,len);
+
     time += clock();
     double total = time / (double) CLOCKS_PER_SEC;
     std::cout << index << std::endl;
-    std::cout << total;
+    std::cout << total << std::endl;
+
     return 0;
 }
