faca49b1515c9c3d393eb542a43478450fbaf7c5
Róger Valderrama <roger.andres18@gmail.com>
2018-04-25 23:10:44 -0600

    Main edited

--- main.cpp
+++ main.cpp
@@ -4,19 +4,20 @@
 #include "Sequential Search/SeqS.h"
 
 int main() {
-    int LargoDeArray = 101;
+    const int LargoDeArray = 10001;
     int array[LargoDeArray] = {};
-    for (int i=0; i<LargoDeArray; i++) {
+    for (int i = 0; i < LargoDeArray; i++) {
         array[i] = i;
     }
     int len = sizeof(array) / sizeof(array[0]);
 
     long time = -clock();
 
-//    int index = BusquedaSecuencial(array, len, LargoDeArray-1); //Secuencial (Lista, largo de lista, Numero a buscar)
-//    int index = binarySearch(array, 0, len, LargoDeArray-1);    //Binaria (Lista, Numero inicial, largo de lista, Numero a buscar)
-    Interpolation interpolation;                                  //Secuencial (Lista, Numero a buscar, largo de lista)
-    int index = interpolation.interpolationSearch(array,LargoDeArray-1,len);
+    //int index = BusquedaSecuencial(array, len, LargoDeArray-1); //Secuencial (Lista, largo de lista, Numero a buscar)
+    int index = binarySearch(array, 0, len,
+                             LargoDeArray - 1);    //Binaria (Lista, Numero inicial, largo de lista, Numero a buscar)
+//    Interpolation interpolation;                                  //Secuencial (Lista, Numero a buscar, largo de lista)
+//    int index = interpolation.interpolationSearch(array,LargoDeArray-1,len);
 
     time += clock();
     double total = time / (double) CLOCKS_PER_SEC;
